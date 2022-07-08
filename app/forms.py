dbcc4e347a1281cfce8725bdaa2374902a4fd95b
Daerenid <patdaw04@gmail.com>
2022-07-08 08:49:41 +0200

    basic schema

---
+++ app/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+
+
+class LoginForm(FlaskForm):
+    pass
+    
+    def validate_username():
+        pass
+
+class RegistrationForm(FlaskForm):
+    pass
+
+    def validate_username():
+        pass
+    
+    def validate_email():
+        pass
+
+    def validate_password():
+        pass
+
+class NewFilm(FlaskForm):
+    pass
+
+class NewCinema(FlaskForm):
+    pass
---
+++ app/models.py
@@ -0,0 +1,28 @@
+from app import db
+
+class Cinema(db.Model):
+    __tablename__ = 'cinema'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(104))
+    films = db.relationship(
+        'Film', backref='cinema', lazy=True
+    )
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(104))
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(104))
+
+class Film(db.Model):
+    __tablename__ = 'film'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(104))
+    repertoire = db.Column(db.String(1048))
+    image_film = db.Column(db.String(50))
+    duration = db.Column(db.Integer)
+    status = db.Column(db.Boolean, default=False)
+    vote_count = db.Integer(db.Column(db.Integer))
+    release_date = db.Column(db.Date)
+    cinema_id = db.Column(db.Integer, db.ForeignKey('cinema.id'))
