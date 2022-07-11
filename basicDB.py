16a5b67e0642c68335df930db0f366c21e4e9629
Daerenid <patdaw04@gmail.com>
2022-07-11 23:33:33 +0200

    new films

--- basicDB.py
+++ basicDB.py
@@ -45,8 +45,69 @@ f4 = Film(
     movieurl = "https://www.filmweb.pl/video/Zwiastun/Mordercza+opona+Zwiastun+nr+1+polski-24067",
     cinema_id = 2
     )
-
+f5 = Film(
+    name="2012", 
+    year = 2009,
+    description="Jackson Curtis próbuje ocalić najbliższych przed niszczącymi Ziemię kataklizmami.",
+    vote_count = 0,
+    posterurl = "https://m.media-amazon.com/images/M/MV5BMTY0MjEyODQzMF5BMl5BanBnXkFtZTcwMTczMjQ4Mg@@._V1_FMjpg_UX1000_.jpg",
+    movieurl = "https://www.youtube.com/watch?v=ce0N3TEcFw0&ab_channel=SonyPicturesEntertainment",
+    cinema_id = 2
+    )
+f6 = Film(
+    name="PITBULL. NIEBEZPIECZNE KOBIETY", 
+    year = 2016,
+    description="Młode policjantki, Zuza i Jadźka dołączają do Pitbulla. Dziewczyny oprócz zmagań w życiu prywatnym muszą stawić czoła przestępczemu światu.",
+    vote_count = 0,
+    posterurl = "https://m.media-amazon.com/images/M/MV5BMTY0MjEyODQzMF5BMl5BanBnXkFtZTcwMTczMjQ4Mg@@._V1_FMjpg_UX1000_.jpg",
+    movieurl = "https://www.youtube.com/watch?v=xZyiL8NgENo&ab_channel=KinoSwiatPL",
+    cinema_id = 2
+    )
+f7 = Film(
+    name="PITBULL. NIEBEZPIECZNE KOBIETY", 
+    year = 2016,
+    description="Młode policjantki, Zuza i Jadźka dołączają do Pitbulla. Dziewczyny oprócz zmagań w życiu prywatnym muszą stawić czoła przestępczemu światu.",
+    vote_count = 0,
+    posterurl = "https://m.media-amazon.com/images/M/MV5BMTY0MjEyODQzMF5BMl5BanBnXkFtZTcwMTczMjQ4Mg@@._V1_FMjpg_UX1000_.jpg",
+    movieurl = "https://www.youtube.com/watch?v=xZyiL8NgENo&ab_channel=KinoSwiatPL",
+    cinema_id = 3
+    )
+f8 = Film(
+    name="Eragon", 
+    year = 2006,
+    description="Eragon – młody, wiejski chłopak znajduje niebieski kamień i przynosi go do domu. Ale zanim udaje mu się sprzedać go handlarzowi, z kamienia wykluwa się szafirowy smok, Saphira. Smoka próbuje ukraść zły Urgals, który brutalnie morduje wuja Eragona. Chłopcu i smoczycy w ostatniej chwili udaje się uciec. Od tej chwili Eragon poprzysięga zemstę mordercy wuja i wyrusza na wyprawę, by uratować świat i stać się ostatnim legendarnym Jeźdźcem Smoków.",
+    vote_count = 0,
+    posterurl = "https://fwcdn.pl/fpo/60/74/196074/7537948.3.jpg",
+    movieurl = "https://www.filmweb.pl/video/Zwiastun/Eragon+Zwiastun+nr+1-17962",
+    cinema_id = 3
+    )
+f9 = Film(
+    name="Król Lew", 
+    year = 2019,
+    description="W wyniku podstępu Skazy prawowity władca afrykańskiej sawanny, Simba, zostaje wygnany. Razem z dwójką przyjaciół zamierza odzyskać tron.",
+    vote_count = 0,
+    posterurl = "https://m.media-amazon.com/images/I/61sJaBqGxZL._AC_SL1184_.jpg",
+    movieurl = "https://www.youtube.com/watch?v=lFzVJEksoDY&ab_channel=DisneyPlus",
+    cinema_id = 3
+    )
+f10 = Film(
+    name="Dzień Niepodległośc", 
+    year = 2019,
+    description="Przedstawiciele pozaziemskiej cywilizacji przypuszczają atak na największe metropolie.",
+    vote_count = 0,
+    posterurl = "https://cdn1.naekranie.pl/media/cache/amp/2014/11/p7254203.5.jpg",
+    movieurl = "https://www.youtube.com/watch?v=qMSA4EaljHU&ab_channel=FilmPlanetPolska",
+    cinema_id = 3
+    )
 c1.add_film(f1)
 c1.add_film(f2)
 c1.add_film(f3)
 c2.add_film(f4)
+c2.add_film(f5)
+c2.add_film(f6)
+c3.add_film(f7)
+c3.add_film(f8)
+c3.add_film(f9)
+c3.add_film(f10)
+
+
