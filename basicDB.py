c355af9eed8415d3e1e3ecac7648da4e4321500e
wxnko <4311891+wxnko@users.noreply.github.com>
2022-07-12 17:05:57 +0200

    change movie urls

--- basicDB.py
+++ basicDB.py
@@ -24,7 +24,7 @@ f2 = Film(
     description="Eragon – młody, wiejski chłopak znajduje niebieski kamień i przynosi go do domu. Ale zanim udaje mu się sprzedać go handlarzowi, z kamienia wykluwa się szafirowy smok, Saphira. Smoka próbuje ukraść zły Urgals, który brutalnie morduje wuja Eragona. Chłopcu i smoczycy w ostatniej chwili udaje się uciec. Od tej chwili Eragon poprzysięga zemstę mordercy wuja i wyrusza na wyprawę, by uratować świat i stać się ostatnim legendarnym Jeźdźcem Smoków.",
     vote_count = 0,
     posterurl = "https://fwcdn.pl/fpo/60/74/196074/7537948.3.jpg",
-    movieurl = "https://www.filmweb.pl/video/Zwiastun/Eragon+Zwiastun+nr+1-17962",
+    movieurl = "https://www.youtube.com/embed/tiriaKj7eLg",
     cinema_id = 1
     )
 f3 = Film(
@@ -42,7 +42,7 @@ f4 = Film(
     description="Posiadająca zdolności parapsychologiczne opona samochodowa obsesyjnie podąża za kobietą, pozbawiając życia każdego, kto stanie jej na drodze.",
     vote_count = 0,
     posterurl = "https://www.kinoluna.pl/sites/default/files/tresc/film/2018/mordercza_opona/poster/mordercza_opona.jpg",
-    movieurl = "https://www.filmweb.pl/video/Zwiastun/Mordercza+opona+Zwiastun+nr+1+polski-24067",
+    movieurl = "https://www.youtube.com/embed/u7bneGeqqVw",
     cinema_id = 2
     )
 f5 = Film(
@@ -78,7 +78,7 @@ f8 = Film(
     description="Eragon – młody, wiejski chłopak znajduje niebieski kamień i przynosi go do domu. Ale zanim udaje mu się sprzedać go handlarzowi, z kamienia wykluwa się szafirowy smok, Saphira. Smoka próbuje ukraść zły Urgals, który brutalnie morduje wuja Eragona. Chłopcu i smoczycy w ostatniej chwili udaje się uciec. Od tej chwili Eragon poprzysięga zemstę mordercy wuja i wyrusza na wyprawę, by uratować świat i stać się ostatnim legendarnym Jeźdźcem Smoków.",
     vote_count = 0,
     posterurl = "https://fwcdn.pl/fpo/60/74/196074/7537948.3.jpg",
-    movieurl = "https://www.filmweb.pl/video/Zwiastun/Eragon+Zwiastun+nr+1-17962",
+    movieurl = "https://www.youtube.com/embed/tiriaKj7eLg",
     cinema_id = 3
     )
 f9 = Film(
